f36c52fc9dee2c4c70458e5f808ec5132c8fa2ee
takasaki91 <y.baton@gmail.com>
2018-08-21 23:47:01 +0900

    add mov_r32_imm32 and short_jump and instructions func pointers and init instructions func pointer.

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
 #include<stdint.h>
 #define MEMORY_SIZE 1024
+uint32_t get_code8(Emulator* emu,int index);
+int32_t get_sign_code8(Emulator*emu, int index);
+uint32_t get_code32(Emulator* emu, int index);
+void mov_r32_imm32(Emulator* emu);
+void short_jump(Emulator* emu);
+typedef void instructions_fuct_t(Emulator*);
+instructions_fuct_t* instructions[256];
+void init_instructions()
+{
+    memset(instructions,0,sizeof(instructions));
+    for(int i = 0;i < 8;i++)
+    {
+        instructions[0xB8 + i] = mov_r32_imm32;
+    }
+    instructions[0xEB] = short_jump;
+}
 enum {
     EAX,
     EBX,
@@ -68,4 +84,15 @@ uint32_t get_code32(Emulator* emu, int index)
         ret |= get_code8(emu,index+i) <<(i**8);
     }
     return ret;
+}
+void mov_r32_imm32(Emulator* emu)
+{
+    uint8_t reg = get_code8(emu,0) - 0xB8;
+    uint32_t value = get_code32(emu,01);
+    emu->registers[reg] = value;
+    emu->eip += 5;
+}
+void short_jump(Emulator* emu){
+    int8_t diff = get_code8(emu,1);
+    emu->eip += (diff+2);
 }
